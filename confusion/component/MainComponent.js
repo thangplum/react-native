d58b5153ab8e028e33675076c21b96de3e45f079
Thang Duc Toan Nguyen <tdtnguyen@knox.edu>
2019-06-19 21:45:22 +0700

    Header

--- confusion/component/MainComponent.js
+++ confusion/component/MainComponent.js
@@ -1,19 +1,36 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
 import { DISHES } from '../shared/dishes';
+import Dishdetail from './DishdetailComponent';
+import { View, Platform } from 'react-native';
+import {createStackNavigator} from 'react-navigation';
+
+const MenuNavigator = createStackNavigator({
+    Menu: { screen: Menu },
+    Dishdetail: { screen: Dishdetail }
+},
+{
+    initialRouteName: 'Menu',
+    navigationOptions: {
+        headerStyle: {
+            backgroundColor: "#512DA8"
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+            color: "#fff"            
+        }
+    }
+}
+);
 
 class Main extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dishes: DISHES
-    };
-  }
 
   render() {
  
     return (
-        <Menu dishes={this.state.dishes} />
+        <View style={{flex: 1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight}}>
+            <MenuNavigator />
+        </View> 
     );
   }
 }
