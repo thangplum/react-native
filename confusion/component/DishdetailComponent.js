1a245034dc6e1c9300e175775a1f2ce471c14e31
Thang Duc Toan Nguyen <tdtnguyen@knox.edu>
2019-06-28 21:37:27 +0700

    Adding modal and form

--- confusion/component/DishdetailComponent.js
+++ confusion/component/DishdetailComponent.js
@@ -1,27 +1,43 @@
-import React, {Component} from 'react';
-import { View, Text, ScrollView, FlatList } from 'react-native';
-import { Card, Icon } from 'react-native-elements';
+import React, { Component } from 'react';
+import { View, Text, ScrollView, FlatList, Modal, Button, StyleSheet } from 'react-native';
+import { Card, Icon, Rating, Input } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import { postFavorite, postComment } from '../redux/ActionCreators';
 
 const mapStateToProps = state => {
     return {
-      dishes: state.dishes,
-      comments: state.comments
+        dishes: state.dishes,
+        comments: state.comments,
+        favorites: state.favorites
     }
 }
+
+const mapDispatchToProps = dispatch => ({
+    postFavorite: (dishId) => dispatch(postFavorite(dishId)),
+    postComment: (dishId, id, rating, author, comment) => dispatch(postComment(dishId, id, rating, author, comment))
+})
+
+
 
 function RenderDish(props) {
     const dish = props.dish;
 
     if (dish != null) {
         return (
-            <Card featuredTitle={dish.name} image={{uri: baseUrl + dish.image}}>
-                <Text style={{margin: 10}}>
-                    {dish.description}
-                </Text>
-                <Icon raised reverse name={props.favorite ? 'heart' : 'heart-o'} type='font-awesome' color='#f50' onPress={() => props.favorite ? console.log('Already favorite') : props.onPress()} />
-            </Card>
+            <View>
+                <Card featuredTitle={dish.name} image={{ uri: baseUrl + dish.image }}>
+                    <Text style={{ margin: 10 }}>
+                        {dish.description}
+                    </Text>
+                    <View style={{ flexDirection: 'row', justifyContent: "center", alignItems: 'center' }}>
+                        <Icon raised reverse name={props.favorite ? 'heart' : 'heart-o'} type='font-awesome' color='#f50' onPress={() => props.favorite ? console.log('Already favorite') : props.onPress()} />
+                        <Icon raised reverse name='pencil' type='font-awesome' color='#512DA8'
+                            onPress={() => props.toggleModal()} />
+                    </View>
+                </Card>
+            </View>
+
         );
     }
     else {
@@ -31,12 +47,14 @@ function RenderDish(props) {
 
 function RenderComments(props) {
     const comments = props.comments;
-    const renderCommentItem = ({item, index}) => {
+    const renderCommentItem = ({ item, index }) => {
         return (
-            <View key={index} style={{margin: 10}}>
-                <Text style={{fontSize: 14}}>{item.comment}</Text>
-                <Text style={{fontSize: 12}}>{item.rating} Stars</Text>
-                <Text style={{fontSize: 12}}>{'--' + item.author + ', ' + item.date}</Text>
+            <View key={index} style={{ margin: 10 }}>
+                <Text style={{ fontSize: 14 }}>{item.comment}</Text>
+                <View style={{ flexDirection: 'row' }}>
+                    <Rating readonly imageSize={10} type='star' startingValue={item.rating}></Rating>
+                </View>
+                <Text style={{ fontSize: 12 }}>{'--' + item.author + ', ' + item.date}</Text>
             </View>
         )
     }
@@ -45,24 +63,45 @@ function RenderComments(props) {
             <FlatList data={comments} renderItem={renderCommentItem} keyExtractor={item => item.id.toString()} />
         </Card>
     )
-} 
+}
 
 class Dishdetail extends Component {
+
     constructor(props) {
         super(props);
         this.state = {
-            favorites: []
+            rating: 5,
+            author: '',
+            comment: '',
+            showModal: false
         }
     }
 
+    static navigationOptions = {
+        title: 'Dish Detail',
+    };
+
+    toggleModal() {
+        this.setState({ showModal: !this.state.showModal });
+    }
+
+    handleComments() {
+        console.log(JSON.stringify(this.state));
+        this.toggleModal();
+    }
+
+    resetForm() {
+        this.setState({ showModal: false })
+    }
+
+    submitForm(dishId) {        
+        this.props.postComment(dishId, this.props.comments.comments.length, this.state.rating, this.state.author, this.state.comment);
+        this.resetForm();
+    }
+
     markFavorite(dishId) {
-        this.setState({favorites: this.state.favorites.concat(dishId)})
+        this.props.postFavorite(dishId);
     }
-    
-    static navigatonOptions = {
-        title: 'Dish Details'
-    }
-    
 
     render() {
         const dishId = this.props.navigation.getParam('dishId', '')
@@ -70,14 +109,73 @@ class Dishdetail extends Component {
         return (
             <ScrollView>
                 <RenderDish dish={this.props.dishes.dishes[+dishId]}
-                    favorite={this.state.favorites.some(el => el === dishId)}
-                    onPress={() => this.markFavorite(dishId)} 
-                    />
+                    favorite={this.props.favorites.some(el => el === dishId)}
+                    onPress={() => this.markFavorite(dishId)}
+                    toggleModal={() => this.setState({ showModal: true })}
+                />
                 <RenderComments comments={this.props.comments.comments.filter((comment) => comment.dishId === dishId)} />
+                <Modal animationType={"slide"} transparent={false}
+                    visible={this.state.showModal}
+                >
+                    <Rating showRating type='star' onFinishRating={(val) => { this.setrating(val) }} startingValue={this.state.rating}></Rating>
+                    <Input
+                        placeholder=' Author'
+                        leftIcon={{ type: 'font-awesome', name: 'user-o' }}
+                        onChangeText={(text) => this.setState({ author: text })}
+                    ></Input>
+                    <Input placeholder=' comment'
+                        leftIcon={{ type: 'font-awesome', name: 'comment-o' }}
+                        onChangeText={(text) => this.setState({ comment: text })}
+                    ></Input>
+                    <View style={styles.modal}>
+                        <Button
+                            onPress={() => { this.toggleModal(); this.submitForm(dishId); }}
+                            color="#512DA8"
+                            title="Submit"
+                        />
+                        <Button
+                            onPress={() => { this.toggleModal(); }}
+                            color="#f50"
+                            title="Cancel"
+                        />
+                    </View>
+                </Modal>
             </ScrollView>
         );
     }
-    
 }
 
-export default connect(mapStateToProps)(Dishdetail);
+const styles = StyleSheet.create({
+    formRow: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+        flexDirection: 'row',
+        margin: 20
+    },
+    formLabel: {
+        fontSize: 18,
+        flex: 2
+    },
+    formItem: {
+        flex: 1
+    },
+    modal: {
+        justifyContent: 'center',
+        margin: 20
+    },
+    modalTitle: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        backgroundColor: '#512DA8',
+        textAlign: 'center',
+        color: 'white',
+        marginBottom: 20
+    },
+    modalText: {
+        fontSize: 18,
+        margin: 10
+    }
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Dishdetail);
--- confusion/component/MainComponent.js
+++ confusion/component/MainComponent.js
@@ -9,21 +9,22 @@ import { createStackNavigator, createDrawerNavigator, DrawerItems, SafeAreaView 
 import { Icon } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { fetchDishes, fetchComments, fetchPromos, fetchLeaders } from '../redux/ActionCreators';
+import Reservation from './ReservationComponent';
 
 const mapStateToProps = state => {
-  return {
-    dishes: state.dishes,
-    comments: state.comments,
-    promotions: state.promotions,
-    leaders: state.leaders
-  }
+    return {
+        dishes: state.dishes,
+        comments: state.comments,
+        promotions: state.promotions,
+        leaders: state.leaders
+    }
 }
 
 const mapDispatchToProps = dispatch => ({
-  fetchDishes: () => dispatch(fetchDishes()),
-  fetchComments: () => dispatch(fetchComments()),
-  fetchPromos: () => dispatch(fetchPromos()),
-  fetchLeaders: () => dispatch(fetchLeaders()),
+    fetchDishes: () => dispatch(fetchDishes()),
+    fetchComments: () => dispatch(fetchComments()),
+    fetchPromos: () => dispatch(fetchPromos()),
+    fetchLeaders: () => dispatch(fetchLeaders()),
 })
 
 const MenuNavigator = createStackNavigator({
@@ -98,21 +99,38 @@ const AboutNavigator = createStackNavigator({
     }
 );
 
+const ReservationNavigator = createStackNavigator({
+    Reservation: { screen: Reservation }
+}, {
+        navigationOptions: ({ navigation }) => ({
+            headerStyle: {
+                backgroundColor: "#512DA8"
+            },
+            headerTitleStyle: {
+                color: "#fff"
+            },
+            headerTintColor: "#fff",
+            headerLeft: <Icon name="menu" size={24}
+                iconStyle={{ color: 'white' }}
+                onPress={() => navigation.navigate('DrawerToggle')} />
+        })
+    })
+
 const CustomDrawerContentComponent = (props) => (
     <ScrollView>
-      <SafeAreaView style={styles.container} forceInset={{ top: 'always', horizontal: 'never' }}>
-        <View style={styles.drawerHeader}>
-          <View style={{flex:1}}>
-          <Image source={require('./images/logo.png')} style={styles.drawerImage} />
-          </View>
-          <View style={{flex: 2}}>
-            <Text style={styles.drawerHeaderText}>Ristorante Con Fusion</Text>
-          </View>
-        </View>
-        <DrawerItems {...props} />
-      </SafeAreaView>
+        <SafeAreaView style={styles.container} forceInset={{ top: 'always', horizontal: 'never' }}>
+            <View style={styles.drawerHeader}>
+                <View style={{ flex: 1 }}>
+                    <Image source={require('./images/logo.png')} style={styles.drawerImage} />
+                </View>
+                <View style={{ flex: 2 }}>
+                    <Text style={styles.drawerHeaderText}>Ristorante Con Fusion</Text>
+                </View>
+            </View>
+            <DrawerItems {...props} />
+        </SafeAreaView>
     </ScrollView>
-  );
+);
 
 const MainNavigator = createDrawerNavigator({
     Home: {
@@ -154,11 +172,26 @@ const MainNavigator = createDrawerNavigator({
                 <Icon name='info-circle' type='font-awesome' size={24} color={tintColor} />
             )
         }
+    },
+    Reservation: {
+        screen: ReservationNavigator,
+        navigationOptions: {
+            title: 'Reserve Table',
+            drawerLabel: 'Reserve Table',
+            drawerIcon: ({ tintColor, focused }) => (
+                <Icon
+                    name='cutlery'
+                    type='font-awesome'
+                    size={24}
+                    iconStyle={{ color: tintColor }}
+                />
+            ),
+        }
     }
 }, {
         drawerBackgroundColor: '#D1C4E9',
         contentComponent: CustomDrawerContentComponent
-});
+    });
 
 class Main extends Component {
     componentDidMount() {
